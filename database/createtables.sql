a77ba261f6925923b53a22c826c6930bbad0a905
washburj10 <52462983+washburj10@users.noreply.github.com>
2019-07-06 19:20:59 -0700

    Created module tables
    modules links module_id to course_id so that modules can be used by several courses
    module_files groups up individual files based on the module_id they belong to

--- database/createtables.sql
+++ database/createtables.sql
@@ -3,3 +3,13 @@ CREATE TABLE employees {
   
   PRIMARY KEY(employee_id)
 };
+
+CREATE TABLE modules {
+  module_id NUMBER(4),
+  course_id NUMBER
+};
+
+CREATE TABLE module_files {
+  file_name VARCHAR2(50) PRIMARY KEY,
+  module_id NUMBER(4) NOT NULL
+};
