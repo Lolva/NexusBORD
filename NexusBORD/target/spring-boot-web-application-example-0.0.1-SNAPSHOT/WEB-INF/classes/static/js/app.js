160150b0c06e72724264e2c7e3dbc629ce49361d
Tyler Campbell <tcampb30@gmail.com>
2019-07-16 03:36:39 -0700

    Cleansed the nuclear waste

--- NexusBORD/target/spring-boot-web-application-example-0.0.1-SNAPSHOT/WEB-INF/classes/static/js/app.js
+++
@@ -1,9 +0,0 @@
-function validate() {
-	var name = document.getElementById("name").value;
-	if (name == '') {
-		alert('Please enter a valid name.');
-		return false;
-	} else {
-		return true;
-	}
-}
