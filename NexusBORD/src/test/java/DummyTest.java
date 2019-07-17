a6950b63b29e61c0afa4ed7e23aad1ca550f57cc
10den11 <domgianatassio@gmail.com>
2019-07-17 14:13:30 -0700

    Added Dummy Test

---
+++ NexusBORD/src/test/java/DummyTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void test() {
+		int actual = 3 * 10;
+		assertEquals(30,actual);
+	}
+
+}
