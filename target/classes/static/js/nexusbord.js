dc1f2e652a2066494abb7d09fab235c6232c2265
SpencerCBacon <spencer_bacon@syntelinc.com>
2019-07-18 22:26:53 -0700

    Login function fixed for new database ***** other queries need to be updated *****

--- target/classes/static/js/nexusbord.js
+++ target/classes/static/js/nexusbord.js
@@ -1,6 +1,6 @@
 function navBar(currentPage, activePage, student ){
 	currentPage.document.getElementById("navDiv").innerHTML = "<ul class='topnavbar'>" +
-			"<li><a class='logout' id ='logout' href='logout' onclick='return confirm('Logout?');'>Logout</a></li>" +
+			"<li><a class='logout' id ='logout' href='logout' onclick=\"return confirm('Logout?');\">Logout</a></li>" +
 			"<li><a id = 'home' href='Nexus.html'>Home</a></li>" +
 			"<li><a id='modules' href='Modules.html'>Modules</a></li>" +
 			"<li><a id='assignments' href='assignments'>Assignments</a></li>" +
