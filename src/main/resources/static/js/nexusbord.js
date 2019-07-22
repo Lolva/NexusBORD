249130c524d9a8bed900998e567a739f8d07842d
Kody Ayers <kayers@email.arizona.edu>
2019-07-22 09:32:44 -0700

    fixed nav not having html

--- src/main/resources/static/js/nexusbord.js
+++ src/main/resources/static/js/nexusbord.js
@@ -5,10 +5,10 @@ var moduleId = null;
 function navBar(currentPage, activePage, student ){
 	currentPage.document.getElementById("navDiv").innerHTML = "<ul class='topnavbar'>" +
 			"<li><a class='logout' id ='logout' href='logout' onclick=\"return confirm('Logout?');\">Logout</a></li>" +
-			"<li><a id = 'home' href='Nexus.html'>Home</a></li>" +
-			"<li><a id='modules' href='Modules.html'>Modules</a></li>" +
-			"<li><a id='assignments' href='InstructorAssignments.html'>Assignments</a></li>" +
-			"<li><a id='classes' href='Classes.html'>Classes</a></li>" +
+			"<li><a id = 'home' href='Nexus'>Home</a></li>" +
+			"<li><a id='modules' href='Modules'>Modules</a></li>" +
+			"<li><a id='assignments' href='InstructorAssignments'>Assignments</a></li>" +
+			"<li><a id='classes' href='Classes'>Classes</a></li>" +
 			"<li class='left'><a class='nexus'><b><font color=white>Nexus</font><font color='#04aad0'>BORD</font></b></a></li>" +
 			"</ul>";
 	
