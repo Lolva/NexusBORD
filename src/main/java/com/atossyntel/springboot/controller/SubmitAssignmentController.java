38db6f91d4d6cab95aa93307a9e3bf120efa25e9
syntel <barnwellrd@gmail.com>
2019-07-21 20:01:01 -0700

    filePath in Student Submission

--- src/main/java/com/atossyntel/springboot/controller/SubmitAssignmentController.java
+++ src/main/java/com/atossyntel/springboot/controller/SubmitAssignmentController.java
@@ -23,6 +23,14 @@ import com.atossyntel.springboot.storage.StorageService;
 @Controller
 public class SubmitAssignmentController {
 	
+	// Instance of the storage location/object
+    private final StorageService storageService;
+    
+    @Autowired
+    public SubmitAssignmentController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+	
 	// object to handle sending of email(s)
     @Autowired
     private SmtpMailSender sms;
@@ -59,11 +67,19 @@ public class SubmitAssignmentController {
 			String assignID = studsub.getAssignment_id();
 			String empID = studsub.getEmployee_id();
 			String filename = file.getOriginalFilename();
+			String streamId = studsub.getStream_id();
+			String moduleId = studsub.getModule_id();
+			String classId = studsub.getClass_id();
 			
 			System.out.println(filename);
 			System.out.println(assignID);
 			System.out.println(empID);
 			
+			//Test case as proof of concept for dynamic folder building
+			StringBuilder modFolder = new StringBuilder("/"+streamId+"/"+classId+"/"+moduleId+"/"+ assignID + "/");
+
+			//within com.atossyntel.springboot.storage.FileSystemStorageService.java
+			storageService.store(file, modFolder.toString());
 
 			studentDAO.submitAssignment("2019-07-21", 90, file, assignID, empID);
 			
