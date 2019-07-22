01c36a8781a3923c737922cfcaede77649d31230
10den11 <domgianatassio@gmail.com>
2019-07-22 09:24:18 -0700

    update to redirect:Nexus

--- src/main/java/com/atossyntel/springboot/controller/LoginController.java
+++ src/main/java/com/atossyntel/springboot/controller/LoginController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.atossyntel.springboot.model.UserLogin;
 import com.atossyntel.springboot.service.LoginDAO;
@@ -24,31 +23,31 @@ public class LoginController {
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-    public Object submit(Model model, @ModelAttribute("login") UserLogin login, HttpSession session) {
-        // First, check that the input isn't null
-        if (login != null && login.getUsername() != null & login.getPassword() != null) {
+	public String submit(Model model, @ModelAttribute("login") UserLogin login, HttpSession session) {
+		// First, check that the input isn't null
+		if (login != null && login.getUsername() != null & login.getPassword() != null) {
 
-            // Next, check that the user exists in the database
-            if (dao.checkPassword(login)) {
+			// Next, check that the user exists in the database
+			if (dao.checkPassword(login)) {
 
-                // Finally, check that the user is enrolled in a class
-                if (dao.isEnrolled(login)) {
-                    session.setAttribute("username", login.getUsername()); // Store the user's username in the session
-                    return new ModelAndView("redirect:/Nexus.html"); // Send the user to the main page
+				// Finally, check that the user is enrolled in a class
+				if (dao.isEnrolled(login)) {
+					session.setAttribute("username", login.getUsername()); // Store the user's username in the session
+					return "redirect:Nexus"; // Send the user to the main page
 
-                } else { // Username has no entry in the Enrollments table
-                    model.addAttribute("error", "You are not enrolled in any classes");
-                    return "login";
-                }
+				} else { // Username has no entry in the Enrollments table
+					model.addAttribute("error", "You are not enrolled in any classes");
+					return "login";
+				}
 
-            } else { // Username and password are not a combination in the database
-                model.addAttribute("error", "Invalid Username or Password");
-                return "login";
-            }
+			} else { // Username and password are not a combination in the database
+				model.addAttribute("error", "Invalid Username or Password");
+				return "login";
+			}
 
-        } else { // User has not entered any input
-            model.addAttribute("error", "Please enter your information");
-            return "login";
-        }
-    }
+		} else { // User has not entered any input
+			model.addAttribute("error", "Please enter your information");
+			return "login";
+		}
+	}
 }
