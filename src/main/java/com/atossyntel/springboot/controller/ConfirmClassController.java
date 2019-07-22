7cc198f305d5bb78b1f24ddfb1448f335c1fdab1
Melda1 <44558371+Melda1@users.noreply.github.com>
2019-07-21 19:40:37 -0700

    Update and rename ConfirmClassControl.java to ConfirmClassController.java

--- src/main/java/com/atossyntel/springboot/controller/ConfirmClassController.java
+++ src/main/java/com/atossyntel/springboot/controller/ConfirmClassController.java
@@ -1,30 +1,37 @@
 package com.atossyntel.springboot.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.atossyntel.springboot.model.ClassBean;
 import com.atossyntel.springboot.service.ClassesDAO;
-import com.atossyntel.springboot.service.ConfirmClassDAO;
+import com.atossyntel.springboot.service.CreateClassDAO;
 
-public class ConfirmClassControl {
+public class ConfirmClassController {
 	@Autowired
 	private ClassesDAO classesdao;
 	
 	@Autowired
-	private ConfirmClassDAO confirmclassdao;
+	private CreateClassDAO createclassdao;
 	
 	@RequestMapping(value = "/ConfirmClass", method = RequestMethod.GET)
-	   public String init(@ModelAttribute("newProduct") ClassBean classToBeAdded) {
-		   if (classToBeAdded != null) {
+	   public String init(@ModelAttribute("ClassBean") ClassBean ClassBean) {
+		   if (ClassBean != null) {
 		   classesdao.getClasses();
 		   return "ConfirmClass";
 	   } else {
 	   	return "CreateClass";
 	   }
+		 
 
 	}
-}
+}	
+	
+
 
