81d5eee42a969c8b9666902147ee1159e8d9e7bf
Melda1 <44558371+Melda1@users.noreply.github.com>
2019-07-21 19:42:32 -0700

    Update CreateClassController.java

--- src/main/java/com/atossyntel/springboot/controller/CreateClassController.java
+++ src/main/java/com/atossyntel/springboot/controller/CreateClassController.java
@@ -1,29 +1,42 @@
 package com.atossyntel.springboot.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import com.atossyntel.springboot.model.ClassBean;
 import com.atossyntel.springboot.service.CreateClassDAO;
+import com.atossyntel.springboot.service.CreateClassDAOService;
 
 @Controller
 public class CreateClassController {
 	@Autowired
 	 CreateClassDAO createclassdao;
 	@RequestMapping(value = "/CreateClass", method = RequestMethod.GET)
-	public String getNewClassForm(@ModelAttribute("newProduct") ClassBean newProduct) {
+	public String getNewClassForm(@ModelAttribute("ClassBean") ClassBean ClassBean) {
 		return "CreateClass";
 	}
 	
 	@RequestMapping(value = "/CreateClass", method = RequestMethod.POST)
-	   public String submit(Model model, @ModelAttribute("newProduct") ClassBean classToBeAdded) {
-		   if (classToBeAdded != null) {
-		   createclassdao.addClasses(classToBeAdded);
-		   model.addAttribute("classToBeAdded", classToBeAdded);
+	   public String submit(Model model, @ModelAttribute("ClassBean") ClassBean ClassBean, HttpSession session) {
+		   if (ClassBean != null) {
+		  createclassdao.addClasses(ClassBean.getClass_Id(),ClassBean.getStream_Id(),ClassBean.getStart_date(),ClassBean.getEnd_date());
+		 //  model.addAttribute("classBean", ClassBean);
 		   return "ConfirmClass";
 	   } else {
 	   	return "CreateClass";
@@ -32,3 +45,4 @@ public class CreateClassController {
 	}
 
 }
+	 
