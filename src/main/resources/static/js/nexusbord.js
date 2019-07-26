dc659e6fcf49ac53d54ddf57e2f384dbfb6d93fe
washburj <washburj.10@gmail.com>
2019-07-26 15:45:20 -0700

    made logout button a modal instead of confirm

--- src/main/resources/static/js/nexusbord.js
+++ src/main/resources/static/js/nexusbord.js
@@ -4,13 +4,33 @@ var moduleId = null;
 
 function navBar(currentPage, activePage, student ){
 	currentPage.document.getElementById("navDiv").innerHTML = "<ul class='topnavbar'>" +
-			"<li><a class='logout' id ='logout' href='logout' onclick=\"return confirm('Logout?');\">Logout</a></li>" +
+			"<li><a class='logout' id='logout' data-toggle='modal' data-target='#logoutModal'><font color=white>Logout</font></a></li>" +
 			"<li><a id = 'home' href='Nexus'>Home</a></li>" +
 			"<li><a id='modules' href='Modules'>Modules</a></li>" +
 			"<li><a id='assignments' href='Assignments'>Assignments</a></li>" +
 			"<li><a id='classes' href='Classes'>Classes</a></li>" +
 			"<li class='left'><a class='nexus'><b><font color=white>Nexus</font><font color='#04aad0'>BORD</font></b></a></li>" +
 			"</ul>";
+	currentPage.document.getElementById("LogoutModalDiv").innerHTML = 
+		"<div class='modal fade' id='logoutModal' tabindex='-1' role='dialog' aria-labelledby='LogoutModalTitle' aria-hidden='true'>" +
+			"<div class='modal-dialog modal-dialog-centered' role='document'>" + 
+				"<div class='modal-content'>" +
+					"<div class='modal-header'>" +
+						"<h5 class='modal-title' id='LogoutModalTitle'>Logout</h5>" +
+						"<button type='button' class='close' data-dismiss='modal' aria-label='Close'>" +
+							"<span aria-hidden='true'>&times;</span>" +
+						"</button>" + 
+					"</div>" +
+					"<div class='modal-body'>" +
+						"<p>Are you sure you want to log out?</p>" +
+					"</div>" +
+					"<div class='modal-footer'>" +
+						"<button type='button' class='btn btn-secondary' data-dismiss='modal'>Cancel</button>" +
+						"<button type='button' class='btn btn-primary' onclick=\"window.location.href='/logout';\">Logout</button>" + 
+					"</div>" +
+				"</div>" +
+			"</div>" +
+		"</div>";
 	
 	currentPage.document.getElementById(activePage).className = "active";
 }
